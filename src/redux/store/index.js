5bf9537b76f665c1acd7a5bad6ce0c0abe760768
SAIKUMAR124 <pavankumar.s12459@gmail.com>
2021-11-17 10:14:45 +0530

    Added redux devtool ext.. & thunk

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,4 +1,6 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 import { expenseReducer } from "../reducers/expenses";
 
 const reducer = combineReducers({
@@ -7,5 +9,5 @@ const reducer = combineReducers({
 
 const initialState = {};
 
-const store = createStore(reducer, initialState);
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 export default store;
